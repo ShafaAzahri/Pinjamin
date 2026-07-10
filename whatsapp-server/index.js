a0a7de9285def73d5d56d8d3fe2a520442e5b1fd
Shafa <azshafa95@gmail.com>
2026-07-10 19:39:38 +0700

    fix: add Puppeteer args for running inside background services and set local userDataDir

--- whatsapp-server/index.js
+++ whatsapp-server/index.js
@@ -18,9 +18,16 @@ const client = new Client({
     authStrategy: new LocalAuth({ dataPath: './whatsapp-session' }),
     puppeteer: {
         headless: true,
+        userDataDir: './chromium-data',
         args: [
             '--no-sandbox',
-            '--disable-setuid-sandbox'
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
         ],
     },
     webVersionCache: {
