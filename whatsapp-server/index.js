4fd93b503e18e56f1b37306144ad4efd6b924dba
Shafa <azshafa95@gmail.com>
2026-07-10 19:57:35 +0700

    fix: remove outdated remote webVersionCache configuration from WhatsApp client options

--- whatsapp-server/index.js
+++ whatsapp-server/index.js
@@ -23,10 +23,6 @@ const client = new Client({
             '--disable-setuid-sandbox',
             '--disable-gpu'
         ],
-    },
-    webVersionCache: {
-        type: 'remote',
-        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
     }
 });
 
